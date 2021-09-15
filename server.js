bf7e58ab1b2d5996af4adcc1bccfda2c6d606fa7
Victor Scherman <vicscherman@gmail.com>
2021-09-15 13:22:46 -0400

    server running

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db')
 const app = express();
+//Connect DB
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('API RUNNING')
