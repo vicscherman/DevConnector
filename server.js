89e4cb9cbb901fb7586f629a4808ae79457e0d44
Victor Scherman <vicscherman@gmail.com>
2021-09-15 17:33:04 -0400

    added user model,tested validation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+//route files
 const usersRoutes = require('./routes/api/users')
 const authRoutes = require('./routes/api/auth')
 const profileRoutes = require('./routes/api/profile')
@@ -8,6 +9,8 @@ const postsRoutes = require('./routes/api/posts')
 
 //Connect DB
 connectDB();
+//init middleware
+app.use(express.json({extended:false}))
 
 app.get('/', (req, res) => {
   res.send('API RUNNING');
