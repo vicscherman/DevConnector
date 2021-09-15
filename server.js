a11e9066d45180c943454d12405de9f51e2fe05c
Victor Scherman <vicscherman@gmail.com>
2021-09-15 13:42:47 -0400

    connected to db, added routes

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 const app = express();
+const usersRoutes = require('./routes/api/users')
+const authRoutes = require('./routes/api/auth')
+const profileRoutes = require('./routes/api/profile')
+const postsRoutes = require('./routes/api/posts')
+
 //Connect DB
-connectDB()
+connectDB();
 
 app.get('/', (req, res) => {
-  res.send('API RUNNING')
-})
+  res.send('API RUNNING');
+});
+
+//Define Routes
+
+app.use('/api/users', usersRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/profile', profileRoutes)
+app.use('/api/posts', postsRoutes)
 
 const PORT = process.env.PORT || 5000;
 
