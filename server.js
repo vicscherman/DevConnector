88c650536f192c487f56915ae991958432b21ff4
Victor Scherman <vicscherman@gmail.com>
2021-09-24 14:05:50 -0400

    prepare to deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path')
+
 const app = express();
 //route files
 const usersRoutes = require('./routes/api/users')
@@ -12,16 +14,23 @@ connectDB();
 //init middleware
 app.use(express.json({extended:false}))
 
-app.get('/', (req, res) => {
-  res.send('API RUNNING');
-});
-
 //Define Routes
 
 app.use('/api/users', usersRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/profile', profileRoutes)
 app.use('/api/posts', postsRoutes)
+
+//Serve static assets in production
+
+if(process.env.NODE_ENV === 'production'){
+  //Set the static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
