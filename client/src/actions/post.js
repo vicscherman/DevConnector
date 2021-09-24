391fbc9fcdb364860ac86597667257c1b135d691
Victor Scherman <vicscherman@gmail.com>
2021-09-24 10:05:09 -0400

    added delete post functionality, reducer, action, type

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES , DELETE_POST} from './types';
 
 //Get Posts
 export const getPosts = () => async (dispatch) => {
@@ -45,6 +45,24 @@ export const removeLike = (postId) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: {postId, likes: res.data}
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//DELETE post
+export const deletePost = (postId) => async (dispatch) => {
+  try {
+     await axios.delete(`/api/posts//${postId}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: postId
+    });
+    dispatch(setAlert('Post Deleted','success'))
   } catch (err) {
     dispatch({
       type: POST_ERROR,
